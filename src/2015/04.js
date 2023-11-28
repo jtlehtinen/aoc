7ff724c98e3392d4646bd5ba3afa22f47b6ec548
Juha Lehtinen <jtlehtinen@fastmail.com>
2023-11-28 21:47:25 +0200

    2015/04

--- src/2015/04.js
+++ src/2015/04.js
@@ -1,17 +1,27 @@
 // https://adventofcode.com/2015/day/4
+import crypto from 'node:crypto'
 import { readFile } from '~/io.js'
 
-const part1 = (s) => {
-  return 0
+/**
+ * @param {string} s
+ * @param {string} prefix
+ * @returns {number}
+ */
+const findKey = (s, prefix) => {
+  const template = crypto.createHash('md5')
+  for (let suffix = 0;; ++suffix) {
+    const hash = template.copy()
+    const digest = hash.update(s + suffix).digest('hex')
+    if (digest.startsWith(prefix)) return suffix
+  }
 }
 
-const part2 = (s) => {
-  return 0
-}
+const part1 = (s) => findKey(s, '00000')
+const part2 = (s) => findKey(s, '000000')
 
 if (import.meta.vitest) {
   const { it, expect } = import.meta.vitest
   const input = readFile('2015/04.txt')
-  it('part 1', () => expect(part1(input)).toBe(0))
-  it('part 2', () => expect(part2(input)).toBe(0))
+  it('part 1', () => expect(part1(input)).toBe(282749))
+  it('part 2', () => expect(part2(input)).toBe(9962624))
 }
