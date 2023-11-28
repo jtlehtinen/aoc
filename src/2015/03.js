eb05a2f2b35727f22b5a0b4e7fe2affa0937706a
Juha Lehtinen <jtlehtinen@fastmail.com>
2023-11-28 21:28:01 +0200

    2015/03

--- src/2015/03.js
+++ src/2015/03.js
@@ -1,17 +1,32 @@
 // https://adventofcode.com/2015/day/3
 import { readFile } from '~/io.js'
 
-const part1 = (s) => {
-  return 0
+const pack = (x, y) => (x + 256) | ((y + 256) << 16)
+
+const deliverPresents = (s, actorCount) => {
+  const x = new Int32Array(actorCount)
+  const y = new Int32Array(actorCount)
+  const visitedHouses = new Set([pack(0, 0)])
+
+  for (const [index, c] of [...s].entries()) {
+    const actor = index % actorCount
+    switch (c) {
+      case '^': y[actor] += 1; break
+      case 'v': y[actor] -= 1; break
+      case '<': x[actor] -= 1; break
+      case '>': x[actor] += 1; break
+    }
+    visitedHouses.add(pack(x[actor], y[actor]))
+  }
+  return visitedHouses.size
 }
 
-const part2 = (s) => {
-  return 0
-}
+const part1 = (s) => deliverPresents(s, 1)
+const part2 = (s) => deliverPresents(s, 2)
 
 if (import.meta.vitest) {
   const { it, expect } = import.meta.vitest
   const input = readFile('2015/03.txt')
-  it('part 1', () => expect(part1(input)).toBe(0))
-  it('part 2', () => expect(part2(input)).toBe(0))
+  it('part 1', () => expect(part1(input)).toBe(2081))
+  it('part 2', () => expect(part2(input)).toBe(2341))
 }
