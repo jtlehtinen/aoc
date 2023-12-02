2784b3d438593dc0688dd02f3aa08a0a3aa2c16d
Juha Lehtinen <jtlehtinen@fastmail.com>
2023-12-02 12:21:31 +0200

    2023/02 compact

--- src/2023/02.js
+++ src/2023/02.js
@@ -1,38 +1,26 @@
 // https://adventofcode.com/2023/day/2
 import { readFile } from '~/io.js'
 
-const parseCubeSet = (s) => {
-  const set = { r: 0, g: 0, b: 0 }
-  for (const [, count, color] of s.matchAll(/(\d+) (\w+)/g)) {
-    set[color[0]] += +count
-  }
-  return set
-}
-
 const parseGames = (s) => {
   return s.split('\n').map((line) => {
-    const [game, ...sets] = line.split(/:|;/)
     return {
-      id: +game.match(/Game (\d+)/)[1],
-      sets: sets.map(parseCubeSet)
+      id: +line.match(/Game (\d+)/)[1],
+      r: Math.max(...Array.from(line.matchAll(/(\d+) red/g),   m => Number(m[1]))),
+      g: Math.max(...Array.from(line.matchAll(/(\d+) green/g), m => Number(m[1]))),
+      b: Math.max(...Array.from(line.matchAll(/(\d+) blue/g),  m => Number(m[1]))),
     }
   })
 }
 
 const part1 = (s) => {
-  const possible = ({ r, g, b }) => r <= 12 && g <= 13 && b <= 14
   return parseGames(s)
-    .filter((game) => game.sets.every(possible))
-    .map((game) => game.id)
+    .filter(({ r, g, b }) => r <= 12 && g <= 13 && b <= 14)
+    .map(({ id }) => id)
     .reduce((sum, id) => sum + id, 0)
 }
 
 const part2 = (s) => {
-  return parseGames(s).map(({ sets }) => ({
-      r: Math.max(...sets.map(set => set.r)),
-      g: Math.max(...sets.map(set => set.g)),
-      b: Math.max(...sets.map(set => set.b)),
-    }))
+  return parseGames(s)
     .map(({ r, g, b }) => r * g * b)
     .reduce((sum, p) => sum + p, 0)
 }
