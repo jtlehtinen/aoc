4d1eac1b26f0d6b5dd156b4c1ea422e165d3407e
Juha Lehtinen <jtlehtinen@fastmail.com>
2023-12-04 17:33:08 +0200

    2017/01

--- src/2017/01.js
+++ src/2017/01.js
@@ -1,17 +1,19 @@
 // https://adventofcode.com/2017/day/1
 import { readFile } from '~/io.js'
 
-const part1 = (s) => {
-  return 0
+const captcha = (s, step) => {
+  const digits = [...s].map(Number)
+  return digits
+    .filter((d, i) => d === digits[(i + step) % digits.length])
+    .reduce((a, b) => a + b, 0)
 }
 
-const part2 = (s) => {
-  return 0
-}
+const part1 = (s) => captcha(s, 1)
+const part2 = (s) => captcha(s, Math.floor(s.length/2))
 
 if (import.meta.vitest) {
   const { it, expect } = import.meta.vitest
   const input = readFile('2017/01.txt')
-  it('part 1', () => expect(part1(input)).toBe(0))
-  it('part 2', () => expect(part2(input)).toBe(0))
+  it('part 1', () => expect(part1(input)).toBe(1175))
+  it('part 2', () => expect(part2(input)).toBe(1166))
 }
