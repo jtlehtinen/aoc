b1e6d3924b204c116af03193a0cb9ff4e33c853f
Juha Lehtinen <jtlehtinen@fastmail.com>
2023-12-04 17:24:23 +0200

    2019/01

--- src/2019/01.js
+++ src/2019/01.js
@@ -1,17 +1,22 @@
 // https://adventofcode.com/2019/day/1
 import { readFile } from '~/io.js'
 
-const part1 = (s) => {
-  return 0
+const parse = (s) => s.split('\n').map(Number)
+
+const fuel = (mass) => Math.max(0, Math.floor(mass / 3) - 2)
+
+const fuelRecursive = (mass) => {
+  if (mass <= 0) return 0
+  const f = fuel(mass)
+  return f + fuelRecursive(f)
 }
 
-const part2 = (s) => {
-  return 0
-}
+const part1 = (s) => parse(s).map(fuel).reduce((a, b) => a + b, 0)
+const part2 = (s) => parse(s).map(fuelRecursive).reduce((a, b) => a + b, 0)
 
 if (import.meta.vitest) {
   const { it, expect } = import.meta.vitest
   const input = readFile('2019/01.txt')
-  it('part 1', () => expect(part1(input)).toBe(0))
-  it('part 2', () => expect(part2(input)).toBe(0))
+  it('part 1', () => expect(part1(input)).toBe(3198599))
+  it('part 2', () => expect(part2(input)).toBe(4795042))
 }
