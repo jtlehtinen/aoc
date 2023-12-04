bb7a0c1fcb8ffbc7b4005ad8bc48baa995aa3c42
Juha Lehtinen <jtlehtinen@fastmail.com>
2023-12-04 17:27:39 +0200

    2018/01

--- src/2018/01.js
+++ src/2018/01.js
@@ -1,17 +1,27 @@
 // https://adventofcode.com/2018/day/1
 import { readFile } from '~/io.js'
 
-const part1 = (s) => {
-  return 0
-}
+const parse = s => s.split('\n').map(Number)
+
+const part1 = (s) => parse(s).reduce((a, b) => a + b, 0)
 
 const part2 = (s) => {
-  return 0
+  const deltas = parse(s)
+
+  let frequency = 0
+  const seen = new Set([ frequency ])
+  for (;;) {
+    for (const d of deltas) {
+      frequency += d
+      if (seen.has(frequency)) return frequency
+      seen.add(frequency)
+    }
+  }
 }
 
 if (import.meta.vitest) {
   const { it, expect } = import.meta.vitest
   const input = readFile('2018/01.txt')
-  it('part 1', () => expect(part1(input)).toBe(0))
-  it('part 2', () => expect(part2(input)).toBe(0))
+  it('part 1', () => expect(part1(input)).toBe(513))
+  it('part 2', () => expect(part2(input)).toBe(287))
 }
