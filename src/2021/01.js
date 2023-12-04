7965d5a2f487c888c050e0a19e02d98516420484
Juha Lehtinen <jtlehtinen@fastmail.com>
2023-12-04 17:12:09 +0200

    2021/01

--- src/2021/01.js
+++ src/2021/01.js
@@ -1,17 +1,25 @@
 // https://adventofcode.com/2021/day/1
 import { readFile } from '~/io.js'
 
-const part1 = (s) => {
-  return 0
+// solve returns the number of times the sum of the sliding
+// window increases when moving from left to right through
+// the array of numbers parsed from s.
+const solve = (s, window) => {
+  const numbers = s.split('\n').map(Number)
+
+  let count = 0
+  for (let i = window; i < numbers.length; ++i) {
+    if (numbers[i] > numbers[i - window]) count++
+  }
+  return count
 }
 
-const part2 = (s) => {
-  return 0
-}
+const part1 = (s) => solve(s, 1)
+const part2 = (s) => solve(s, 3)
 
 if (import.meta.vitest) {
   const { it, expect } = import.meta.vitest
   const input = readFile('2021/01.txt')
-  it('part 1', () => expect(part1(input)).toBe(0))
-  it('part 2', () => expect(part2(input)).toBe(0))
+  it('part 1', () => expect(part1(input)).toBe(1195))
+  it('part 2', () => expect(part2(input)).toBe(1235))
 }
