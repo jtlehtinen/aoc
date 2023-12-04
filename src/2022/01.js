fda92b8eccd93cad8d8dbefd32ad535c987e1a89
Juha Lehtinen <jtlehtinen@fastmail.com>
2023-12-04 17:05:12 +0200

    2022/01

--- src/2022/01.js
+++ src/2022/01.js
@@ -1,17 +1,32 @@
 // https://adventofcode.com/2022/day/1
 import { readFile } from '~/io.js'
 
+// parse returns the number of calories carried by each elf
+// @param {string} s
+const parse = (s) => {
+  return s.split('\n\n')
+    .map(section => {
+      return section
+        .match(/\d+/g)
+        .map(Number)
+        .reduce((a, b) => a + b, 0)
+    })
+}
+
 const part1 = (s) => {
-  return 0
+  return Math.max(...parse(s))
 }
 
 const part2 = (s) => {
-  return 0
+  return parse(s)
+    .sort((a, b) => b - a)
+    .slice(0, 3)
+    .reduce((a, b) => a + b, 0)
 }
 
 if (import.meta.vitest) {
   const { it, expect } = import.meta.vitest
   const input = readFile('2022/01.txt')
-  it('part 1', () => expect(part1(input)).toBe(0))
-  it('part 2', () => expect(part2(input)).toBe(0))
+  it('part 1', () => expect(part1(input)).toBe(69912))
+  it('part 2', () => expect(part2(input)).toBe(208180))
 }
