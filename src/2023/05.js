be61a93e4c9abb4acdded8b3d11b518a0d8ea103
Juha Lehtinen <jtlehtinen@fastmail.com>
2023-12-05 13:26:34 +0200

    2023/05 tweaks

--- src/2023/05.js
+++ src/2023/05.js
@@ -1,63 +1,57 @@
 // https://adventofcode.com/2023/day/5
 import { readFile } from '~/io.js'
 
-const parseNumbers = (s) => s.match(/(\d+)/g).map(Number)
-
-const parseMap = (s) => {
-  const [, srcCategory, dstCategory] =  s.match(/(\w+)-to-(\w+)/)
-
-  const mappings = s.split('\n').slice(1).map(line => {
-    const [dst, src, len] = parseNumbers(line)
-    return { lower: src, upper: src + len - 1, delta: dst - src }
-  })
-
-  return { srcCategory, dstCategory, mappings }
-}
-
 const parse = (s) => {
   const [seeds, ...maps] = s.split('\n\n')
   return {
-    seeds: parseNumbers(seeds),
-    maps: maps.map(parseMap),
+    seeds: seeds.match(/(\d+)/g).map(Number),
+    maps: maps.map(s => {
+      const [, srcCategory, dstCategory] =  s.match(/(\w+)-to-(\w+)/)
+      return {
+        srcCategory,
+        dstCategory,
+        mappings: s.split('\n').slice(1).map(line => {
+          const [dst, src, len] = line.match(/\d+/g).map(Number)
+          return { lower: src, upper: src + len - 1, delta: dst - src }
+        }),
+      }
+    }),
   }
 }
 
 const solve = (ranges, maps) => {
-  const result = []
+  let minLocation = Number.MAX_SAFE_INTEGER
   const queue = ranges.map(r => ({ srcCategory: 'seed', ...r }))
 
   while (queue.length > 0) {
     const { srcCategory, r0, r1 } = queue.pop()
     if (srcCategory === 'location') {
-      result.push({ r0, r1 })
+      minLocation = Math.min(minLocation, r0)
       continue
     }
 
     const map = maps.find(m => m.srcCategory === srcCategory)
     const mapping = map.mappings.find(({ lower, upper }) => !(r1 < lower || r0 > upper))
 
-    if (!mapping) {
+    if (mapping) {
+      const { lower, upper, delta } = mapping
+      if (r0 < lower) queue.push({ srcCategory, r0, r1: lower-1 })
+      if (r1 > upper) queue.push({ srcCategory, r0: upper+1, r1 })
+      queue.push({
+        srcCategory: map.dstCategory,
+        r0: Math.max(r0, lower) + delta,
+        r1: Math.min(r1, upper) + delta,
+      })
+    } else {
       queue.push({ srcCategory: map.dstCategory, r0, r1 })
-      continue
     }
-    const { lower, upper, delta } = mapping
-
-    if (r0 < lower) queue.push({ srcCategory, r0, r1: lower-1 })
-    if (r1 > upper) queue.push({ srcCategory, r0: upper+1, r1 })
-
-    queue.push({
-      srcCategory: map.dstCategory,
-      r0: Math.max(r0, lower) + delta,
-      r1: Math.min(r1, upper) + delta,
-    })
   }
-
-  return Math.min(...result.map(({ r0 }) => r0))
+  return minLocation
 }
 
 const part1 = (s) => {
   const { seeds, maps } = parse(s)
-  const ranges = seeds.map(seed => ({ r0: seed, r1: seed })) // range of 1
+  const ranges = seeds.map(seed => ({ r0: seed, r1: seed }))
   return solve(ranges, maps)
 }
 
@@ -65,7 +59,7 @@ const part2 = (s) => {
   const { seeds, maps } = parse(s)
 
   const ranges = []
-  for (let i = 0; i < seeds.length; i += 2) { // start0, len0, start1, len1, ...
+  for (let i = 0; i < seeds.length; i += 2) {
     ranges.push({ r0: seeds[i], r1: seeds[i] + seeds[i+1] - 1 })
   }
 
