f13b7dd60bed3a75cec0eda0a40bf74f58e68f11
Juha Lehtinen <jtlehtinen@fastmail.com>
2023-12-07 20:09:39 +0200

    2017/02

--- src/2017/02.js
+++ src/2017/02.js
@@ -1,17 +1,37 @@
 // https://adventofcode.com/2017/day/2
 import { readFile } from '~/io.js'
 
+const parse = (s) => s.split('\n').map(line => line.split(/\s+/).map(Number))
+
+const findEvenlyDivisibleValues = (array) => {
+  for (let i = 0; i < array.length; ++i) {
+    for (let j = i + 1; j < array.length; ++j) {
+      const a = array[i]
+      const b = array[j]
+      if (a % b === 0) return [a, b]
+      if (b % a === 0) return [b, a]
+    }
+  }
+}
+
 const part1 = (s) => {
-  return 0
+  return parse(s)
+    .map(nums => Math.max(...nums) - Math.min(...nums))
+    .reduce((a, b) => a + b, 0)
 }
 
 const part2 = (s) => {
-  return 0
+  return parse(s)
+    .map(nums => {
+      const [a, b] = findEvenlyDivisibleValues(nums)
+      return a / b
+    })
+    .reduce((a, b) => a + b, 0)
 }
 
 if (import.meta.vitest) {
   const { it, expect } = import.meta.vitest
   const input = readFile('2017/02.txt')
-  it('part 1', () => expect(part1(input)).toBe(0))
-  it('part 2', () => expect(part2(input)).toBe(0))
+  it('part 1', () => expect(part1(input)).toBe(41919))
+  it('part 2', () => expect(part2(input)).toBe(303))
 }
