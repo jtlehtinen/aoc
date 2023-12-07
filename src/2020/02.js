a475c09a4e55a31a8eeeb5a7e35c8376916cfb4c
Juha Lehtinen <jtlehtinen@fastmail.com>
2023-12-07 19:43:02 +0200

    2020/02

--- src/2020/02.js
+++ src/2020/02.js
@@ -1,17 +1,31 @@
 // https://adventofcode.com/2020/day/2
 import { readFile } from '~/io.js'
 
+const parse = (s) => {
+  return s.split('\n').map(line => {
+    const [, lo, hi, char, password] = line.match(/(\d+)-(\d+) (\w): (\w+)/)
+    return { lo: +lo, hi: +hi, char, password }
+  })
+}
+
 const part1 = (s) => {
-  return 0
+  return parse(s)
+    .filter(({ lo, hi, char, password }) => {
+      const occurrences = [...password].filter(c => c === char).length
+      return occurrences >= lo && occurrences <= hi
+    }).length
 }
 
 const part2 = (s) => {
-  return 0
+  return parse(s)
+    .filter(({ lo, hi, char, password }) =>
+      (password[lo-1] === char) !== (password[hi-1] === char))
+    .length
 }
 
 if (import.meta.vitest) {
   const { it, expect } = import.meta.vitest
   const input = readFile('2020/02.txt')
-  it('part 1', () => expect(part1(input)).toBe(0))
-  it('part 2', () => expect(part2(input)).toBe(0))
+  it('part 1', () => expect(part1(input)).toBe(580))
+  it('part 2', () => expect(part2(input)).toBe(611))
 }
