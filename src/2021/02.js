5f33853490e8b98127a5746c76b896e797b87077
Juha Lehtinen <jtlehtinen@fastmail.com>
2023-12-07 19:39:30 +0200

    2021/02

--- src/2021/02.js
+++ src/2021/02.js
@@ -1,17 +1,44 @@
 // https://adventofcode.com/2021/day/2
 import { readFile } from '~/io.js'
 
+const parse = (s) => {
+  return s.split('\n').map(line => {
+    const [action, count] = line.split(' ')
+    return { action, count: +count }
+  })
+}
+
+const simulate = (s, dispatch) => {
+  let x = 0
+  let d = 0
+  let aim = 0
+  for (const { action, count } of parse(s)) {
+    [x, d, aim] = dispatch[action](count, x, d, aim)
+  }
+  return x * d
+}
+
 const part1 = (s) => {
-  return 0
+  const dispatch = {
+    'forward': (count, x, d, aim) => [x+count, d, aim],
+    'down':    (count, x, d, aim) => [x, d+count, aim],
+    'up':      (count, x, d, aim) => [x, d-count, aim],
+  }
+  return simulate(s, dispatch)
 }
 
 const part2 = (s) => {
-  return 0
+  const dispatch = {
+    'forward': (count, x, d, aim) => [x+count, d + aim*count, aim],
+    'down':    (count, x, d, aim) => [x, d, aim+count],
+    'up':      (count, x, d, aim) => [x, d, aim-count],
+  }
+  return simulate(s, dispatch)
 }
 
 if (import.meta.vitest) {
   const { it, expect } = import.meta.vitest
   const input = readFile('2021/02.txt')
-  it('part 1', () => expect(part1(input)).toBe(0))
-  it('part 2', () => expect(part2(input)).toBe(0))
+  it('part 1', () => expect(part1(input)).toBe(1670340))
+  it('part 2', () => expect(part2(input)).toBe(1954293920))
 }
