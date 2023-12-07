da00f7c8d44cd2c7996edbe79a4971fcf0d0fc0d
Juha Lehtinen <jtlehtinen@fastmail.com>
2023-12-07 20:06:11 +0200

    2018/02

--- src/2018/02.js
+++ src/2018/02.js
@@ -2,16 +2,45 @@
 import { readFile } from '~/io.js'
 
 const part1 = (s) => {
-  return 0
+  let two = 0
+  let three = 0
+
+  s.split('\n').forEach(line => {
+    const charToCount = new Map()
+    Array.from(line).forEach(c => charToCount.set(c, (charToCount.get(c) ?? 0) + 1))
+
+    const counts = Array.from(charToCount.values())
+    if (counts.some(c => c === 2)) two++
+    if (counts.some(c => c === 3)) three++
+  })
+
+  return two * three
 }
 
 const part2 = (s) => {
-  return 0
+  const sets = s.split('\n').map(id => {
+    const set = new Set()
+    Array.from(id).forEach((c , i) => set.add(c.charCodeAt(0) | (i << 8)))
+    return set
+  })
+
+  for (let i = 0; i < sets.length; ++i) {
+    for (let j = i + 1; j < sets.length; ++j) {
+      const setA = sets[i]
+      const setB = sets[j]
+
+      let common = ''
+      setA.forEach(value => {
+        if (setB.has(value)) common += String.fromCharCode(value & 0xff)
+      })
+      if (common.length === setA.size-1) return common
+    }
+  }
 }
 
 if (import.meta.vitest) {
   const { it, expect } = import.meta.vitest
   const input = readFile('2018/02.txt')
-  it('part 1', () => expect(part1(input)).toBe(0))
-  it('part 2', () => expect(part2(input)).toBe(0))
+  it('part 1', () => expect(part1(input)).toBe(6972))
+  it('part 2', () => expect(part2(input)).toBe('aixwcbzrmdvpsjfgllthdyoqe'))
 }
