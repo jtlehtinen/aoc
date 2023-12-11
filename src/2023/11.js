6aa35372a4f6619fd2813d4d305e88f346539c2e
Juha Lehtinen <jtlehtinen@fastmail.com>
2023-12-11 12:15:17 +0200

    2023/11 make faster

--- src/2023/11.js
+++ src/2023/11.js
@@ -5,43 +5,48 @@ const sumShortestPath = (map, expansion) => {
   const w = map[0].length
   const h = map.length
 
-  const galaxies = []
-  const galaxyRows = new Set()
-  const galaxyCols = new Set()
+  const galaxiesX = []
+  const galaxiesY = []
+  const galaxyRows = new Array(h).fill(1) // 1 empty, 0 galaxy
+  const galaxyCols = new Array(w).fill(1) // 1 empty, 0 galaxy
 
   for (let y = 0; y < h; ++y) {
     for (let x = 0; x < w; ++x) {
       if (map[y][x] === '#') {
-        galaxies.push({ x, y })
-        galaxyCols.add(x)
-        galaxyRows.add(y)
+        galaxiesX.push(x)
+        galaxiesY.push(y)
+        galaxyCols[x] = 0
+        galaxyRows[y] = 0
       }
     }
   }
 
-  const cumEmptyCols = new Array(w).fill(0)
-  const cumEmptyRows = new Array(h).fill(0)
-  for (let x = 0; x < w; ++x) cumEmptyCols[x] = cumEmptyCols.at(x - 1) + (galaxyCols.has(x) ? 0 : 1)
-  for (let y = 0; y < h; ++y) cumEmptyRows[y] = cumEmptyRows.at(y - 1) + (galaxyRows.has(y) ? 0 : 1)
+  const cumEmptyCols = new Array(w).fill(0) // first col doesn't matter
+  const cumEmptyRows = new Array(h).fill(0) // first row doesn't matter
+  for (let x = 1; x < w; ++x) cumEmptyCols[x] = cumEmptyCols[x - 1] + galaxyCols[x]
+  for (let y = 1; y < h; ++y) cumEmptyRows[y] = cumEmptyRows[y - 1] + galaxyRows[y]
 
-  const n = galaxies.length
-  const distances = new Array(n * n).fill(0)
+  let emptyColSum = 0
+  let emptyRowSum = 0
+  let lenSum = 0
 
-  for (let i = 0; i < galaxies.length; ++i) {
-    const a = galaxies[i]
+  const N = galaxiesX.length
+  for (let i = 0; i < N - 1; ++i) {
+    const ax = galaxiesX[i]
+    const ay = galaxiesY[i]
 
-    for (let j = i + 1; j < galaxies.length; ++j) {
-      const b = galaxies[j]
-      const dx = Math.abs(a.x - b.x)
-      const dy = Math.abs(a.y - b.y)
+    const emptyColsAx = cumEmptyCols[ax]
+    const emptyRowsAy = cumEmptyRows[ay]
 
-      const emptyCols = Math.abs(cumEmptyCols[a.x] - cumEmptyCols[b.x])
-      const emptyRows = Math.abs(cumEmptyRows[a.y] - cumEmptyRows[b.y])
-
-      distances[i + j * n] = dx + dy + (emptyCols + emptyRows) * (expansion - 1)
+    for (let j = i + 1; j < N; ++j) {
+      const bx = galaxiesX[j]
+      const by = galaxiesY[j]
+      emptyColSum += Math.abs(emptyColsAx - cumEmptyCols[bx])
+      emptyRowSum += Math.abs(emptyRowsAy - cumEmptyRows[by])
+      lenSum += Math.abs(ax - bx) + Math.abs(ay - by)
     }
   }
-  return distances.reduce((a, b) => a + b, 0)
+  return lenSum + (emptyColSum + emptyRowSum) * (expansion - 1)
 }
 
 const parse = (s) => s.split('\n').map(line => Array.from(line))
