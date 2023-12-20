9610ef405ef11f5e02a3bad926d16abd6080ca2c
Juha Lehtinen <jtlehtinen@fastmail.com>
2023-12-20 16:50:05 +0200

    2022/06

--- src/2022/06.js
+++ src/2022/06.js
@@ -1,17 +1,20 @@
 // https://adventofcode.com/2022/day/6
 import { readFile } from '~/io.js'
 
-const part1 = (s) => {
-  return 0
+const firstMarker = (s, window) => {
+  for (let i = window; i <= s.length; ++i) {
+    const ss = s.substring(i - window, i)
+    if (new Set(Array.from(ss)).size == window)
+      return i
+  }
 }
 
-const part2 = (s) => {
-  return 0
-}
+const part1 = (s) => firstMarker(s, 4)
+const part2 = (s) => firstMarker(s, 14)
 
 if (import.meta.vitest) {
   const { it, expect } = import.meta.vitest
   const input = readFile('2022/06.txt')
-  it('part 1', () => expect(part1(input)).toBe(0))
-  it('part 2', () => expect(part2(input)).toBe(0))
+  it('part 1', () => expect(part1(input)).toBe(1804))
+  it('part 2', () => expect(part2(input)).toBe(2508))
 }
