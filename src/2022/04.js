40e3f6075d57957d06c016a6db6cf15330eef2d7
Juha Lehtinen <jtlehtinen@fastmail.com>
2023-12-20 16:21:59 +0200

    2022/04

--- src/2022/04.js
+++ src/2022/04.js
@@ -1,17 +1,23 @@
 // https://adventofcode.com/2022/day/4
 import { readFile } from '~/io.js'
 
+const parse = (s) => s.split('\n').map(line => line.match(/\d+/g).map(Number))
+
 const part1 = (s) => {
-  return 0
+  return parse(s)
+    .filter(([a, b, c, d]) => (a >= c && b <= d) || (c >= a && d <= b))
+    .length
 }
 
 const part2 = (s) => {
-  return 0
+  return parse(s)
+    .filter(([a, b, c, d]) => (b >= c && a <= d))
+    .length
 }
 
 if (import.meta.vitest) {
   const { it, expect } = import.meta.vitest
   const input = readFile('2022/04.txt')
-  it('part 1', () => expect(part1(input)).toBe(0))
-  it('part 2', () => expect(part2(input)).toBe(0))
+  it('part 1', () => expect(part1(input)).toBe(569))
+  it('part 2', () => expect(part2(input)).toBe(936))
 }
